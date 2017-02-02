21915b2b7310233ebee2e71bc072b7c994df15a5
Tikhon Jelvis <tikhon.jelvis@target.com>
2017-02-01 23:33:19 -0800

    Cabal- and Nixified the whole talks repo.

---
+++ shell.nix
@@ -0,0 +1,30 @@
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default" }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, base, Chart, Chart-diagrams, containers
+      , diagrams, diagrams-contrib, diagrams-lib, diagrams-svg, fgl
+      , interpolate, mtl, stdenv
+      }:
+      mkDerivation {
+        pname = "talks";
+        version = "0.1.0.0";
+        src = ./.;
+        libraryHaskellDepends = [
+          base Chart Chart-diagrams containers diagrams diagrams-contrib
+          diagrams-lib diagrams-svg fgl interpolate mtl
+        ];
+        license = stdenv.lib.licenses.unfree;
+      };
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  drv = haskellPackages.callPackage f {};
+
+in
+
+  if pkgs.lib.inNixShell then drv.env else drv
